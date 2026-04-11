f89b6c92be54106da46db83756173816f4051949
SrinithishB <srinithishofficial@gmail.com>
2026-04-12 00:15:41 +0530

    DB fix

--- build.sh
+++ build.sh
@@ -4,3 +4,16 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser from env vars (skips if already exists)
+if [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+  python manage.py shell -c "
+from django.contrib.auth import get_user_model
+U = get_user_model()
+if not U.objects.filter(email='$DJANGO_SUPERUSER_EMAIL').exists():
+    U.objects.create_superuser(email='$DJANGO_SUPERUSER_EMAIL', password='$DJANGO_SUPERUSER_PASSWORD')
+    print('Superuser created.')
+else:
+    print('Superuser already exists, skipping.')
+"
+fi
