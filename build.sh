6463ef139c9710089071b9702090fa5be25b481d
SrinithishB <srinithishofficial@gmail.com>
2026-06-07 13:17:56 +0530

    deploy: add --clear flag to collectstatic in build.sh to purge stale static assets

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic --clear --no-input
 python manage.py migrate
 
 # Create superuser from env vars (skips if already exists)
